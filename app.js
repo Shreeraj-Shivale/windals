0a998805a9f7276fc3cbbe1e808889e702236746
Shreeraj Shivale <66412849+Shreeraj-Shivale@users.noreply.github.com>
2024-09-24 06:41:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,22 +5,29 @@ const app = express();
 
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const cors = require("cors"); // Import the 'cors' middleware
+const cors = require("cors");
 
-// Regular middleware
+//regular middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("build"));
 app.use(express.static(path.join(__dirname, "build")));
 
-// Other middleware
+// other middleware
 app.use(cookieParser());
 app.use(morgan("tiny"));
+app.use(cors());
 
-// Enable CORS for all routes
-app.use(cors()); // Add this line to allow cross-origin requests
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.header('Access-Control-Max-Age', '86400'); // Cache preflight requests for 24 hours
+//     next();
+//   });
+  
 
-// Define your routes
+// all routes
 const user = require("./routes/user");
 const defect = require("./routes/defect");
 const process = require("./routes/process");
@@ -28,7 +35,7 @@ const plantCode = require("./routes/plantCode");
 const productionLine = require("./routes/productionLine");
 const report = require("./routes/report");
 
-// Router middleware
+// router middleware
 app.use("/api/v1", user);
 app.use("/api/v1", defect);
 app.use("/api/v1", process);
@@ -36,14 +43,12 @@ app.use("/api/v1", plantCode);
 app.use("/api/v1", productionLine);
 app.use("/api/v1", report);
 
-// Serve your frontend (index.html)
 app.get("*", async (req, res) => {
     try {
-        res.sendFile(path.join(__dirname, "build", "index.html"));
-    } catch (err) {
+        res.sendFile(path.join(__dirname, "build", "index.html"))
+    } catch(err) {
         console.log(err);
-        res.status(500).send("Internal Server Error");
     }
-});
+})
 
 module.exports = app;
